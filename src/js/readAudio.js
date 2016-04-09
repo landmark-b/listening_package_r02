63c31148b9eaffdd453cd13220f60a70a470e3f4
umeume1234 <guilbaija@yahoo.co.jp>
2016-04-09 16:16:52 +0900

    First commit after create repository
    This commit add the local working directory.
    This is included the index.html and so on.

---
+++ src/js/readAudio.js
@@ -0,0 +1,17 @@
+function readAudio(audio)
+{
+	switch (audio){
+	case "audio1-1":
+		src = "../audio/anne_greengables_librivox/anne_of_green_gables_01_montgomery_64kb.mp3";
+		break;
+	
+	case "audio1-2":
+		src = "../audio/anne_greengables_librivox/anne_of_green_gables_02_montgomery_64kb.mp3"
+		break;
+	
+	default:
+		break;
+	}
+
+
+}
